7ba2b6d889676ae201c0c805487b63599ce26fc3
Parambir Singh <paramstr@gmail.com>
2025-12-27 19:55:22 +1300

    testing shit

--- plugins/withPodfileDeploymentTarget.js
+++ plugins/withPodfileDeploymentTarget.js
@@ -4,26 +4,41 @@ const withPodfileDeploymentTarget = (config) => {
   return withPodfile(config, (config) => {
     const podfileContent = config.modResults.contents;
 
-    // Avoid duplicating the patch if it's already there
-    if (podfileContent.includes("Compliance for some pods")) {
+    // Avoid duplicating the patch
+    if (podfileContent.includes("target.name == 'NitroModules'")) {
       return config;
     }
 
-    // The code we want to inject to force minimum deployment target on all Pods
     const patch = `
-    # Compliance for some pods (like SDWebImage) that have old deployment targets
-    installer.pods_project.targets.each do |target|
+  installer.pods_project.targets.each do |target|
+
+    # SDWebImage fix
+    if target.name.include?('SDWebImage')
       target.build_configurations.each do |config|
-        if config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'].to_f < 18.0
-          config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '18.0'
-        end
+        config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '18.0'
       end
     end
+
+    # NitroModules fix
+    if target.name == 'NitroModules'
+      target.build_configurations.each do |config|
+        config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '18.0'
+        config.build_settings['SWIFT_VERSION'] = '5.9'
+      end
+    end
+
+    # Unistyles fix (Swift + availability)
+    if target.name == 'Unistyles'
+      target.build_configurations.each do |config|
+        config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '18.0'
+        config.build_settings['SWIFT_VERSION'] = '5.9'
+      end
+    end
+
+  end
 `;
 
-    // We interpolate this into the `post_install` block.
-    // We'll look for `post_install do |installer|` and insert our patch right after it.
-    // This runs it at the beginning of post_install.
+
     const modifiedContent = podfileContent.replace(
       /post_install do \|installer\|/,
       `post_install do |installer|${patch}`
